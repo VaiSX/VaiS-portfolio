3b06abb7bed5337f3a15e7950fbe5053d46f7693
VaiS <vaibhav.1807singh@gmail.com>
2024-04-23 02:53:01 +0530

    fix

--- src/components/transitionProvider.jsx
+++ src/components/transitionProvider.jsx
@@ -21,7 +21,7 @@ const TransitionProvider = ({ children }) => {
           transition={{ duration: 0.5, ease: "easeOut" }}
         />
         <motion.div
-          className="fixed mx-auto my-[45%] top-0 bottom-0 left-0 right-0 text-white text-8xl cursor-default z-50 w-fit h-fit md:my-auto"
+          className="fixed mx-auto my-[25%] top-0 bottom-0 left-0 right-0 text-white text-8xl cursor-default z-50 w-fit h-fit md:my-auto"
           initial={{ opacity: 1 }}
           animate={{ opacity: 0 }}
           exit={{ opacity: 0 }}
